54f45dd9977db40e7f39476b51dd926fe55a7da9
Théo Martin <martin.to@orange.fr>
2025-05-22 12:09:05 +0200

    Ajout favicon et image OpenGraph

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "09bbf51445a5a73be4814075c2416ea8
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "3a01c3c0ae4113f2535a7576e8de5dea",
+"flutter_bootstrap.js": "0dace1610436a0de8edeebfa3fc96eb3",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
