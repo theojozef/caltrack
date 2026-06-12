889dfddf77f8338a40ab210c78214ea0f0e13052
Théo Martin <martin.to@orange.fr>
2026-06-12 15:25:20 +0200

    Mise à jour flexfood.ctv2

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2898330668"
+    serviceWorkerVersion: "4184349821"
   }
 });
--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba00f08bb10fd64c78121b21093f9e60
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "7b761016b97166c1f0be24befde25ea6",
+"flutter_bootstrap.js": "5e925a236a55294a20efe4492dd6864b",
 "icon-caltrack-32.png": "bd5def2552a9229938514d71195c4bc9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
@@ -50,7 +50,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ba00f08bb10fd64c78121b21093f9e60
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "f3f30d6d59cd2ca63680e563d12c35c1",
 "/": "f3f30d6d59cd2ca63680e563d12c35c1",
-"main.dart.js": "153f13bbb85be4cdbbfc5c14cfa79ed5",
+"main.dart.js": "630a123b50edb4adc33dfa013981b5cd",
 "manifest.json": "334128aeaacf8c6aecd290251210403d",
 "version.json": "363b64fcf8b4cc98ba6b271c311d4d51"};
 // The application shell files that are downloaded before a service worker can
